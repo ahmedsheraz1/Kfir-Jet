4d04409b2962104f7980a4a8d9ee52986826a303
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-13 23:26:09 +0500

    Create Kfir Jet.py

---
+++ Kfir Jet.py
@@ -0,0 +1,5 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+from hashlib import blake2b
+blake2b(b'Kfir Jet').hexdigest()
+'058ce45d9b89f9f388cbe69f1ce7ca1856b0e6a1ed66ce7a594c464d6b2c9d2ea1673ea5b0fde69fdecbb64d026c8cef0ebcc558d78f75f38981bab2ab5ea204'
